ca5ef415672804e0c09004a467a7a893ea2efd43
Muhammed Ali Nayir <malinayir@gmail.com>
2019-07-07 16:28:15 +0300

    Update bustadicebot.js

--- bustadicebot.js
+++ bustadicebot.js
@@ -1,7 +1,7 @@
 var config = {
-  baseBet: { value: 20, type: 'balance', label: 'base bet' },
-  payout: { value: 2.60, type: 'multiplier' },
-  max: { value: 1e8, type: 'balance', label: 'Max bet' },
+  baseBet: { value: 100, type: 'balance', label: 'base bet' },
+  payout: { value: 2.17, type: 'multiplier' },
+  max: { value: 1e8, type: 'balance', label: 'en yüksek bet' },
 
 };
 
@@ -16,8 +16,8 @@ var count = 0;
 
 setTimeout(function start(){
     engine.bet(roundBit(currentBet), config.payout.value);
-    engine.on('GAME_STARTING', onGameStarted);
-    engine.on('GAME_ENDED', onGameEnded);
+ engine.on('GAME_STARTING', onGameStarted);
+engine.on('GAME_ENDED', onGameEnded);
 }, 1000 );
 
 
@@ -39,8 +39,30 @@ function onGameEnded() {
       count =0;
     log('count = ' , count );
   } else {
-  currentBet=currentBet*2;
-  
+    // damn, looks like we lost :(
+    if ( count < 2) {
+      currentBet = config.baseBet.value;
+      count++;
+      log('count = ' , count );
+    } else if(count == 2){
+       count++;
+
+      currentBet = config.baseBet.value * 10;
+      log('count = ' , count );
+    }
+ else if(count == 3){
+    count++;
+
+   currentBet = config.baseBet.value * 25;
+   log('count = ' , count );
+ }
+else if(count ==11){
+count=0}
+    else {
+        count++;
+        currentBet= currentBet * 2;
+        log('count = ' , count );
+    }
     log('We lost, so next bet will be', currentBet/100, 'bits')
 
   }
