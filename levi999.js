51cb2c15e5b3fa11ca202e2d2a07563bd6082389
Muhammed Ali Nayir <malinayir@gmail.com>
2019-07-18 20:25:58 +0300

    Update levi999.js

--- levi999.js
+++ levi999.js
@@ -1,95 +1,92 @@
 var config = {
-  baseBet: { value: 5000, type: 'balance', label: 'base bet' },
-  payout: { value: 3, type: 'multiplier' },
-  max: { value: 1e8, type: 'balance', label: 'max Bet' },
+    baseBet: { value: 100, type: 'balance', label: 'base bet' },
+    payout: { value: 2.17, type: 'multiplier' },
+    max: { value: 1e8, type: 'balance', label: 'en yüksek bet' },
+  
+  };
+  
+  
+  console.log('%c Hello levi script is starting ', 'color: #2D872D');
+  
+  var currentBet = config.baseBet.value;
+  var count = 0;
+  var profit = 0;
+  var totalcount=0;
 
-};
-
-
-log('Script is running..');
-
-var currentBet = config.baseBet.value;
-var count = 1;
-var count1 = 1;
-var a = 0;
-var b = 1;
-
-// Always try to bet when script is started
-
-
-setTimeout(function start(){
-    if(count1==1){
-        engine.bet(roundBit(currentBet), config.payout.value);
-        engine.on('GAME_STARTING', onGameStarted);
-        engine.on('GAME_ENDED', onGameEnded);
+  
+  // Always try to bet when script is started
+  
+  
+  setTimeout(function start(){
+      engine.bet(roundBit(currentBet), config.payout.value);
+      engine.on('GAME_STARTING', onGameStarted);
+      engine.on('GAME_ENDED', onGameEnded);
+  }, 1000 );
+  
+  
+  function onGameStarted() { 
+      engine.bet(roundBit(currentBet), config.payout.value);
     }
-    count1=1;
-}, 1000 );
-
-
-function onGameStarted() { 
-    if(count1==1){
-        engine.bet(roundBit(currentBet), config.payout.value);
+  
+  function onGameEnded() {
+    var lastGame = engine.history.first()
+  
+    // If we wagered, it means we played
+    if (!lastGame.wager) {
+      return;
     }
-    count1=1;
-  }
-
-function onGameEnded() {
-  var lastGame = engine.history.first()
-
-  // If we wagered, it means we played
-  if (!lastGame.wager) {
-    return;
-  }
-
-  // we won..
-  if (lastGame.cashedAt) {
-      count1=0
-      count =0;
-   
+  
+    // we won..
+    if (lastGame.cashedAt) {
+        console.log("%c Game Win !! Profit = ",'color: #2D872D', (currentBet*config.payout.value)/100 ,"Total profit = "  , profit )
+        profit+=(currentBet*config.payout.value)/100
+        currentBet = config.baseBet.value;
+        console.log("%c Game Count = ",'color: #ff5500' , totalcount)
+        totalcount++;
+        count =0;
+    } else {
+      // damn, looks like we lost :(
+      if ( count < 2) {
+        profit-=currentBet/100
+        currentBet = config.baseBet.value;
+        count++;
+        totalcount++;
+        console.log("%c Game lost Now bet = ",'color: #e60000', currentBet/100, " Total profit = "  , profit)
+        
+      } else if(count == 2){
+         count++;
+         totalcount++;
+         profit-=currentBet/100
+        currentBet = config.baseBet.value * 10;
+        console.log("%c Game lost Now bet = ",'color: #e60000', currentBet/100 , "%k Total profit = " , profit)
+      }
+   else if(count == 3){
+      count++;
+      totalcount++;
+      profit-=currentBet/100
+     currentBet = config.baseBet.value * 25;
+     console.log("%c Game lost Now bet = ",'color: #e60000', currentBet/100, "%k Total profit = ", profit )
+   }
+  else if(count ==11){
+  count=0}
+      else {
+          profit-=currentBet/100
+          count++;
+          totalcount++;
+          currentBet= currentBet * 2;
+          console.log("%c Game lost - Bet = " ,'color: #e60000', currentBet/100, "%k Total profit = ", profit)
+  
+      }
+     
+    }
+  
+    if (currentBet > config.max.value) {
       currentBet = config.baseBet.value;
-    log('count = ' , count );
-  } else if(count<1){
-    count1=1;
-    count++
-    currentBet=config.baseBet.value;
-    log('We lost, so next bet will be', currentBet/100, 'bits')
-
-  }else if (count ==1){
-    count1=1;
-    count++
-    currentBet=config.baseBet.value*2;
-    log('We lost, so next bet will be', currentBet/100, 'bits')
+      console.log("%c Fail Fail Fail !!!! ",'color: #e60000'  )
+    }
   }
-  else if (count ==2){
-    count++
-    count1=1;
-    currentBet=config.baseBet.value*3;
-    log('We lost, so next bet will be', currentBet/100, 'bits')
+  
+  function roundBit(bet) {
+    return Math.round(bet / 100) * 100;
   }
-  else if (count ==3){
-    count++
-    count1=1;
-    currentBet=config.baseBet.value*5;
-    log('We lost, so next bet will be', currentBet/100, 'bits')
-  }
-  else if (count ==4){
-    count1=1;
-    currentBet=config.baseBet.value*6;
-    count++
-    log('We lost, so next bet will be', currentBet/100, 'bits')
-  }else{
-    count++
-    count1=1;
-    currentBet=currentBet*2;
-    log('We lost, so next bet will be', currentBet/100, 'bits')
-  }
-
-  if (currentBet >= config.max.value) {
-    currentBet = config.baseBet.value;
-  }
-}
-
-function roundBit(bet) {
-  return Math.round(bet / 100) * 100;
-}
+                            
